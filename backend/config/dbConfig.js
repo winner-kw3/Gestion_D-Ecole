1db4a04e6af17433c2655ccc14390c37ebf57dd6
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-06-23 16:11:10 +0200

    Update du backend

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'db5018089052.hosting-data.io',
+  user: process.env.DB_USER || 'dbu2150858',
+  password: process.env.DB_PASSWORD || 'GestionEcole2025',
+  database: process.env.DB_NAME || 'dbs14364186',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
